7b74c6e31183ed14cb99f3a98568eea6eab28e1b
sahil duhan <sahilduhan_se20b9_12@dtu.ac.in>
2023-07-10 01:34:17 +0530

    git update

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api", (req, res) => {
+  res.send("hey there!");
+});
